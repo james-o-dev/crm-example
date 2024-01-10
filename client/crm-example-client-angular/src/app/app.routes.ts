752457a1bf78f400d18959a4e1016a0a849d6aef
James O Dev <james.o.dev@gmail.com>
2024-01-11 10:56:31 +1300

    refactor(client-angular): Remove unneeded pathMatch from routes

--- client/crm-example-client-angular/src/app/app.routes.ts
+++ client/crm-example-client-angular/src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router'
 
 export const routes: Routes = [
-  { path: 'sign-up', pathMatch: 'full', loadComponent: () => import('./feature/sign-up/sign-up.component').then(m => m.SignUpComponent) },
-  { path: 'sign-in', pathMatch: 'full', loadComponent: () => import('./feature/sign-in/sign-in.component').then(m => m.SignInComponent) },
-  { path: 'home', pathMatch: 'full', loadComponent: () => import('./feature/home/home.component').then(m => m.HomeComponent) },
-  { path: 'user-profile', pathMatch: 'full', loadComponent: () => import('./feature/user-profile/user-profile.component').then(m => m.UserProfileComponent) },
+  { path: 'sign-up', loadComponent: () => import('./feature/sign-up/sign-up.component').then(m => m.SignUpComponent) },
+  { path: 'sign-in', loadComponent: () => import('./feature/sign-in/sign-in.component').then(m => m.SignInComponent) },
+  { path: 'home', loadComponent: () => import('./feature/home/home.component').then(m => m.HomeComponent) },
+  { path: 'user-profile', loadComponent: () => import('./feature/user-profile/user-profile.component').then(m => m.UserProfileComponent) },
   // Default route; Will redirect to here if a path is not found.
-  { path: '**', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'home'  },
 ]
