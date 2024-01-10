f006da4b0695e8bab2a01af52c603883befb5999
James O Dev <james.o.dev@gmail.com>
2024-01-10 22:22:46 +1300

    docs(client-angular): Adds/updates comments

--- client/crm-example-client-angular/src/app/core/auth.service.ts
+++ client/crm-example-client-angular/src/app/core/auth.service.ts
@@ -9,11 +9,29 @@ export class AuthService {
 
   constructor() { }
 
+  /**
+   * Request to sign up a user.
+   * * TODO: Placeholder
+   *
+   * @param {string} email
+   */
   signUp(email: string) {
+    // From mock DB.
     return from(signUpEndpoint(email))
+    // API Endpoint.
+    // TODO
   }
 
+  /**
+   * Request to sign in a user.
+   * * TODO: Placeholder
+   *
+   * @param {string} email
+   */
   signIn(email: string) {
+    // From mock DB.
     return from(signInEndpoint(email))
+    // API Endpoint.
+    // TODO
   }
 }
--- client/crm-example-client-angular/src/app/core/mock.ts
+++ client/crm-example-client-angular/src/app/core/mock.ts
@@ -1,17 +1,31 @@
+/**
+ * Default empty mock DB.
+ */
 const newDbSchema = JSON.stringify({
   users: [],
   contacts: [],
   tasks: [],
 })
 
+/**
+ * Get the mock DB.
+ */
 export const getLocalStorageDb = () => {
   return JSON.parse(localStorage.getItem('db') || newDbSchema)
 }
 
+/**
+ * Save / override the mock DB
+ *
+ * @param {object} newDb
+ */
 export const saveLocalStorageDb = (newDb: object) => {
   localStorage.setItem('db', JSON.stringify(newDb))
 }
 
+/**
+ * Remove/reset the mock DB from localStorage.
+ */
 export const clearLocalStorageDb = () => {
-  localStorage.clear()
+  localStorage.removeItem('db')
 }
