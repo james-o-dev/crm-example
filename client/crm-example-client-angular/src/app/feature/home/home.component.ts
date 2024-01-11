c4d467ad51d793f1243d83e907797e7274c7e7de
James O Dev <james.o.dev@gmail.com>
2024-01-11 13:31:17 +1300

    feat(client-angular): Adds home FAB, to support future routes

--- client/crm-example-client-angular/src/app/feature/home/home.component.ts
+++ client/crm-example-client-angular/src/app/feature/home/home.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core'
 import { LayoutComponent } from '../../shared/layout/layout.component'
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button'
+import { MatMenuModule } from '@angular/material/menu'
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     LayoutComponent,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
