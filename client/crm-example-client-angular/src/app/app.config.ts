cafe3fb2f06ab7310fa02dde3080a34f47ec9b9b
James O Dev <james.o.dev@gmail.com>
2024-01-18 18:41:56 +1300

    style(client-angular): Linting

--- client/crm-example-client-angular/src/app/app.config.ts
+++ client/crm-example-client-angular/src/app/app.config.ts
@@ -2,12 +2,12 @@ import { ApplicationConfig, isDevMode } from '@angular/core'
 import { provideRouter } from '@angular/router'
 
 import { routes } from './app.routes'
-import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideAnimations } from '@angular/platform-browser/animations'
 import { provideServiceWorker } from '@angular/service-worker'
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideAnimations(), provideServiceWorker('ngsw-worker.js', {
         enabled: !isDevMode(),
-        registrationStrategy: 'registerWhenStable:30000'
+        registrationStrategy: 'registerWhenStable:30000',
     })],
 }
