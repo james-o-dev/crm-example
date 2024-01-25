7290be756c8a6803f64773cd8271c4f21dd98aec
James O Dev <james.o.dev@gmail.com>
2024-01-25 14:00:12 +1300

    perf(database-sql): Add pgsql indexes to foreign key columns

--- database/schema/1_tables.sql
+++ database/schema/1_tables.sql
@@ -30,13 +30,14 @@ CREATE TABLE public.contacts (
 	phone varchar NULL,
 	notes varchar NULL,
 	user_id uuid NOT NULL,
-	archived boolean NULL DEFAULT FALSE,
+	archived bool NULL DEFAULT false,
 	date_created bigint NOT NULL DEFAULT now_unix_timestamp(),
 	date_modified bigint NOT NULL DEFAULT now_unix_timestamp(),
 	CONSTRAINT contacts_pk PRIMARY KEY (contact_id),
 	CONSTRAINT contacts_unique UNIQUE (email),
 	CONSTRAINT contacts_users_fk FOREIGN KEY (user_id) REFERENCES public.users(user_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
+CREATE INDEX contacts_user_id_idx ON public.contacts USING btree (user_id);
 
 
 -- public.tasks definition
@@ -58,3 +59,5 @@ CREATE TABLE public.tasks (
 	CONSTRAINT tasks_contacts_fk FOREIGN KEY (contact_id) REFERENCES public.contacts(contact_id) ON DELETE CASCADE ON UPDATE CASCADE,
 	CONSTRAINT tasks_users_fk FOREIGN KEY (user_id) REFERENCES public.users(user_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
+CREATE INDEX tasks_contact_id_idx ON public.tasks USING btree (contact_id);
+CREATE INDEX tasks_user_id_idx ON public.tasks USING btree (user_id);
