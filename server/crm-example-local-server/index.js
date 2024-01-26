bf6738350b184b3e24ca8c43a88edd10a61ee959
James O Dev <james.o.dev@gmail.com>
2024-01-26 16:33:18 +1300

    refactor(server-local): Use port from env var, else use the default

--- server/crm-example-local-server/index.js
+++ server/crm-example-local-server/index.js
@@ -11,7 +11,7 @@ import { searchEndpoint } from './services/search.js'
 import { dashboardDataEndpoint } from './services/dashboard.js'
 
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 const HOST = 'localhost'
 
 app.use(express.json())
