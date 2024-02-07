448b00ed6ae17ea5cc570d0d6af4397251fa9cd2
James O Dev <james.o.dev@gmail.com>
2024-02-08 10:40:13 +1300

    test(tests-server): Adds 'export-contacts-json' tests

---
+++ tests/server/server-tests/tests/import-export/export-contacts-json.spec.js
@@ -0,0 +1,48 @@
+const { commonHeaders } = require('../../lib/common')
+const { signUpNewUser } = require('../../lib/common.auth')
+const { createNewContact } = require('../../lib/common.contacts')
+
+describe('Export Contacts JSON tests', () => {
+  let user
+
+  const exportContactsRequest = async (accessToken) => {
+    return fetch(`${process.env.API_HOST}/export/contacts/json`, {
+      headers: commonHeaders(accessToken),
+    })
+  }
+
+  beforeAll(async () => {
+    user = await signUpNewUser()
+  })
+
+  // Successfully get search results.
+  test('Successfully get search results', async () => {
+    let data, response
+
+    // Create new contact.
+    const contact = await createNewContact(user.accessToken)
+    const contact2 = await createNewContact(user.accessToken)
+
+    response = await exportContactsRequest(user.accessToken)
+    data = await response.json()
+    expect(response.status).toBe(200)
+    expect(data.json).toBeTruthy()
+
+    const exported = JSON.parse(data.json)
+    expect(exported.length).toBe(2)
+    expect(exported.some(c => c.name === contact.name && c.email === contact.email)).toBe(true)
+    expect(exported.some(c => c.name === contact2.name && c.email === contact2.email)).toBe(true)
+  })
+
+  // Invalid token.
+  test('Invalid access token', async () => {
+    const accessTokens = ['', null, user.refreshToken]
+
+    return Promise.all(accessTokens.map(async (accessToken) => {
+      const response = await exportContactsRequest(accessToken)
+      const data = await response.json()
+      expect(response.status).toBe(401)
+      expect(data.message).toBe('Unauthorized.')
+    }))
+  })
+})
