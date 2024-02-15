15ebeb482d775d0a56549f14835c3cd7a785585a
James O Dev <james.o.dev@gmail.com>
2024-02-16 08:59:49 +1300

    test(tests-server): Increase default test timeout

--- tests/server/server-tests/jest.config.js
+++ tests/server/server-tests/jest.config.js
@@ -2,4 +2,5 @@ module.exports = {
   testEnvironment: 'node',
   setupFiles: ['dotenv/config'], // Load dotenv for environment variables
   globalTeardown: './lib/teardownJest.js',
+  testTimeout: 10000, // Increase this, depending on server and/or database configurations (i.e. increase if they are slow).
 }
